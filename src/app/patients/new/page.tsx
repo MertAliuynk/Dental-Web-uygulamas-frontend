2128a0c53f37a761a45610698ea0b3490fe1e592
MertAliuynk <mertuynk3@gmail.com>
2025-08-09 00:03:01 +0300

    fix: useSearchParams kullanan tüm page'lere force-dynamic eklendi, Next.js 15 build hatası engellendi

--- src/app/patients/new/page.tsx
+++ src/app/patients/new/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 import NewPatientPageClient from "./NewPatientPageClient";
 import AppLayout from "../../components/AppLayout";
 
